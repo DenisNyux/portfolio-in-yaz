2c0440fc8709dd260a627ff5d53319542b51871c
DenisNyux <54911993+DenisNyux@users.noreply.github.com>
2020-12-15 01:54:18 +0300

    added tasks

---
+++ tasks/Диагностические задания/Dz.md
@@ -0,0 +1,4 @@
+# Диагностические задания
+
+## Задание 1
+&#0F00; &#0F0E - the seed syllable for the Body of all buddhas
--- tasks/задание 3/sas.js
+++
@@ -1,6 +0,0 @@
-const fw = '\u0F00\u0F0E',
-sw = '\u0F42\u0F53\u0F66\u0F0B\u0F5A\u0F74\u0F63\u0F0C\u0F0D'
-
-
-console.log(fw)
-console.log(sw)
