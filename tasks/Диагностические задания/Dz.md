82b2bbb50a2fc29695aa70d31c277ca3d3f730e6
DenisNyux <54911993+DenisNyux@users.noreply.github.com>
2020-12-15 02:33:32 +0300

    added tasks

--- tasks/Диагностические задания/Dz.md
+++ tasks/Диагностические задания/Dz.md
@@ -1,4 +1,8 @@
 # Диагностические задания
 
 ## Задание 1
-&#0F00; &#0F0E - the seed syllable for the Body of all buddhas
+&#x0F00;&#x0F0E; - the seed syllable for the Body of all buddhas
+
+&#x0F42;&#x0F53;&#x0F66;&#x0F0B;&#x0F5A;&#x0F74;&#x0F63;&#x0F0C;&#x0F0D; - how things are, manner of being, situation, condition, circumstances, existential self, reality, nature, being-in-itself
+
+## Задание 3
--- tasks/Диагностические задания/task1.js
+++
@@ -1,6 +0,0 @@
-const fw = '\u0F00\u0F0E - the seed syllable for the Body of all buddhas\n',
-sw = '\u0F42\u0F53\u0F66\u0F0B\u0F5A\u0F74\u0F63\u0F0C\u0F0D - how things are, manner of being, situation, condition, circumstances, existential self, reality, nature, being-in-itself';
-
-document.querySelector('body').appendChild(
-    document.createElement("span").appendChild(document.createTextNode(fw + '\n' + sw)));
-
